6bbcdcd02892b2838cd7b0f2f43a36e4cdec71f0
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-12 13:49:10 +0500

    Conditional JS Finished Vlues for Practice

---
+++ Data Structures and Algorithms/Conditionals/app1.js
@@ -0,0 +1,25 @@
+//Consider the situation in which you are required to check
+//if a number is 1 or 0. If it's 1 you need to print true. 
+//Otherwise, you should print false.
+
+let x = 1
+
+if (x == 1) {
+    console.log('true')
+        
+} else {
+    console.log('false')
+        
+}
+
+/*Was checking for false
+let x = 0
+
+if (x == 1) {
+    console.log('true')
+        
+} else {
+    console.log('false')
+        
+}
+*/
