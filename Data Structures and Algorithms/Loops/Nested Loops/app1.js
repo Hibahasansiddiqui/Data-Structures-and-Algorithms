0ed1dd9afe3d2d8fcff380accdb88793da85692d
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-23 19:46:51 +0500

    Completed Nested Loop Task 3

---
+++ Data Structures and Algorithms/Loops/Nested Loops/app1.js
@@ -0,0 +1,24 @@
+/*Pose you’re given an array [ [1, 2], [3, 4], [5, 6] ]. 
+Print all the members of this two-dimensional array using for loop 
+and while nested loops.*/
+
+let arr = [ [1, 2], [3, 4], [5, 6] ]
+
+
+//using for loop
+ /*for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      console.log(arr[i][j]);
+    }
+  }*/
+
+  //using while loop
+  let i=0;
+  while(i<array.length){ 
+    let j=0;
+   while(j<2){
+      console.log(array[i][j]);
+      j++;
+   }
+   i++;
+  }
