0cf8eb2efae35d050ae277bbb239d7d87ba29542
Hibahasansiddiqui <hiba.hasan.siddiqui@gmail.com>
2021-06-27 12:54:14 +0500

    practiced strings js coding

---
+++ Data Structures and Algorithms/Strings/strings.js
@@ -0,0 +1,12 @@
+let name = "Mamdani"
+
+//Strings are subscriptable or indexable
+console.log(name[name.length-1]);
+
+//Strings
+console.log(name [name.length-6])
+
+//We can run loops on strings
+for (let i=0; i<name.length; i++) {
+    console.log(name[i], 'String characters')
+}
